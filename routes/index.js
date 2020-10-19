02f2244a33ccb4e207523c13e22ebad7e3eec879
Elena Quintana <elenaquintana10@gmail.com>
2020-10-19 11:38:03 +0100

    version 2 con la pag principal modificada

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RadioQuintana', title2: 'La mejor radio de Canarias', title3: 'Elena Quintana Tarajano', title4: 'TING'});
 });
 
 module.exports = router;
