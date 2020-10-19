b5b628009ba3e7908318dc502c3392d23757248c
Elena Quintana <elenaquintana10@gmail.com>
2020-10-19 12:05:30 +0100

    version 3 ruta servicios

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//**
+
+var miRutaRouter = require('./routes/servicios');
+
+//**
+
 var app = express();
 
 // view engine setup
@@ -21,6 +27,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//**
+
+app.use('/servicios', miRutaRouter);
+
+//**
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/servicios.js
+++ routes/servicios.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RadioQuintana', title2: 'La mejor radio de Canarias', title3: 'Elena Quintana Tarajano', title4: 'Autora:' });
+  res.render('servicios', { title: 'Servicios', title2: 'de la mejor radio de Canarias', title3: 'Elena', title4: 'RadioQuintana' });
 });
 
 module.exports = router;
