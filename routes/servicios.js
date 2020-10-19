97c781316a0c502453cb9473c74b4295c0ae693d
Elena Quintana <elenaquintana10@gmail.com>
2020-10-19 12:09:38 +0100

    version 3.1 con servicios

--- routes/servicios.js
+++ routes/servicios.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('servicios', { title: 'Servicios', title2: 'de la mejor radio de Canarias', title3: 'Elena', title4: 'RadioQuintana' });
+  res.render('servicios', { title: 'Servicios', title2: 'La mejor radio de Canarias', title3: 'Programación, radio on´line, radio en directo en la frecuencia 94.4, grabaciones y edición de audio', title4: 'RadioQuintana' });
 });
 
 module.exports = router;
